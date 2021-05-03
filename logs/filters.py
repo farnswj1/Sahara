048cb3716830b2213bf8de3deaf15f8f26d51c86
Justin Farnsworth <farnswj1@tcnj.edu>
2021-05-03 13:06:48 -0400

    fixed typos in log filter

--- logs/filters.py
+++ logs/filters.py
@@ -9,8 +9,8 @@ ACTIONS = (
 
 class LogFilter(FilterSet):
     action = ChoiceFilter("action", label="Action", lookup_expr="exact", choices=ACTIONS)
-    model_name = CharFilter("weight", label="Minimum weight", lookup_expr="icontains")
-    model_id = NumberFilter("weight", label="Maximum weight", lookup_expr="exact")
+    model_name = CharFilter("model_name", label="Model Name", lookup_expr="icontains")
+    model_id = NumberFilter("model_id", label="Model ID", lookup_expr="exact")
 
     class Meta:
         model = Log
